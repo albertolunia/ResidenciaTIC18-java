428814f2074db417b5034c6be615e605c2d94853
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-11-27 10:26:02 -0300

    feat: estrutura base do usuario

--- Aula10/src/instaDegas/Usuario.java
+++ Aula10/src/instaDegas/Usuario.java
@@ -1,5 +1,56 @@
 package instaDegas;
 
+import java.util.ArrayList;
+
 public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+    private ArrayList<Integer> idAmigos;
+    private ArrayList<String> postagens;
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        idAmigos = new ArrayList<Integer>();
+        postagens = new ArrayList<String>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public ArrayList<Integer> getIdAmigos() {
+        return idAmigos;
+    }
+
+    public void novaPostagem(String texto) {
+       
+    }
+
+    public void elistaPostagens() {
+       
+    }
+
+    public void exibirFeed() {
+        //mostrar amigos , quantidade de postes
+    }
+
+    public void criarAmizade(int idAmigo) {
+        idAmigos.add(idAmigo);
+    }
+
+
+
+    
     
 }
