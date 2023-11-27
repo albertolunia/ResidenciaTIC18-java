b95052743903f5618e8c158c0ad0fc2cbea6f852
albertolunia <albertolunia@gmail.com>
2023-11-27 10:24:43 -0300

    Menu completo

--- Aula10/src/instaDegas/RedeSocial.java
+++ Aula10/src/instaDegas/RedeSocial.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 public class RedeSocial {
     private ListaSessao listaSessao;
     private ListaUsuarios listaUsuario;
+
+    public static void menu(){
+        System.out.println("1 - Novo usuário");
+        System.out.println("2 - Remove post");
+        System.out.println("3 - Logar");
+        System.out.println("0 - Sair");
+    }
+
+    public static void menuSessao(){
+        System.out.println("1 - Listar postagem");
+        System.out.println("2 - Criar postagem");
+        System.out.println("3 - Criar amizade");
+        System.out.println("4 - Desfazer amizade");
+        System.out.println("0 - Menu anterior");
+
+    }
     
     public static void main(String[] args) throws Exception {
         Scanner nc = new Scanner(System.in);
-        int opcao = 0;
-        int opcaoSessao = 0;
+        int opcao = 0, opcaoSessao = 0;
 
-        
         do{
             menu();
             opcao = nc.nextInt();
@@ -23,6 +37,29 @@ public class RedeSocial {
                     break;
                 case 3:
                     System.out.println("Logar");
+                    do{
+                        menuSessao();
+                        opcaoSessao = nc.nextInt();
+                        switch(opcaoSessao){
+                            case 1:
+                                System.out.println("Listar postagem");
+                                break;
+                            case 2:
+                                System.out.println("Criar postagem");
+                                break;
+                            case 3:
+                                System.out.println("Criar amizade");
+                                break;
+                            case 4:
+                                System.out.println("Desfazer amizade");
+                                break;
+                            case 0:
+                                System.out.println("Menu anterior");
+                                break;
+                            default:
+                                System.out.println("Opção inválida");
+                        }
+                    }while(opcaoSessao != 0);
                     break;
                 case 0:
                     System.out.println("Sair");
