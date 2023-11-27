b0bbd33b195c4fd2b3fbb73d2184699d26a0d0ee
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-11-27 11:11:56 -0300

    feat: gets e sets

--- Aula10/src/instaDegas/Usuario.java
+++ Aula10/src/instaDegas/Usuario.java
@@ -1,13 +1,17 @@
 package instaDegas;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
+
 public class Usuario {
+    private int id;
     private String nome;
     private String email;
     private String senha;
     private ArrayList<Integer> idAmigos;
     private ArrayList<String> postagens;
+    
 
     public Usuario(String nome, String email, String senha) {
         this.nome = nome;
@@ -17,6 +21,9 @@ public class Usuario {
         postagens = new ArrayList<String>();
     }
 
+    public int getId() {
+        return id;
+    }
     public String getNome() {
         return nome;
     }
@@ -29,28 +36,58 @@ public class Usuario {
         return senha;
     }
 
+    public void setNome(String novoNome) {
+        nome = novoNome;
+    }
+
+    public void setEmail(String novoEmail) {
+        email = novoEmail;
+    }
+
+    public void setSenha(String novaSenha) {
+        senha = novaSenha;
+    }
+
     public ArrayList<Integer> getIdAmigos() {
         return idAmigos;
     }
 
     public void novaPostagem(String texto) {
+        postagens.add(texto);
+        System.out.println("Postagem realizada com sucesso!");
        
     }
 
-    public void elistaPostagens() {
+    public void listaPostagens() {
+        for (int i = 0; i < postagens.size(); i++) {
+            System.out.println(postagens.get(i));
+        }
        
     }
 
     public void exibirFeed() {
         //mostrar amigos , quantidade de postes
+        System.out.println("Nome: " + nome);
+        System.out.println("Email: " + email);
+        System.out.println("Quantidade de amigos: " + idAmigos.size());
+        System.out.println("Quantidade de postagens: " + postagens.size());
     }
 
     public void criarAmizade(int idAmigo) {
         idAmigos.add(idAmigo);
     }
 
+    public boolean logar() {
+        Sessao sessao = new Sessao(this);
+
+    }
+
+    public LocalDate dataCadastro() {
+        LocalDate data = LocalDate.now();
+        return data;
+    }
 
 
-    
+
     
 }
