07de36594b7a07aef240789eace7473a65c80364
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-27 10:18:29 -0300

    feat: add listas

--- Aula10/src/instaDegas/RedeSocial.java
+++ Aula10/src/instaDegas/RedeSocial.java
@@ -1,5 +1,8 @@
 package instaDegas;
 public class RedeSocial {
+    private ListaSessao listaSessao;
+    private ListaUsuarios listaUsuario;
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
