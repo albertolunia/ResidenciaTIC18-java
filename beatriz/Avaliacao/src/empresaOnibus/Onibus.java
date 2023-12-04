ed78d70ee36b31d71237301a3805c50c1767d007
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-04 14:07:41 -0300

    feat: descrição dos métodos na class onibus

--- beatriz/Avaliacao/src/empresaOnibus/Onibus.java
+++ beatriz/Avaliacao/src/empresaOnibus/Onibus.java
@@ -1,6 +1,8 @@
 package empresaOnibus;
 
 public class Onibus extends Veiculo{
+	private int qtdPassageirosEstudantil;
+	private int qtdPassageirosIdoso;
 	private int qtdPassageiros;
 
 	public Onibus(String placa, int qtdAcentos, String modelo) {
@@ -8,7 +10,9 @@ public class Onibus extends Veiculo{
 		this.qtdPassageiros = 0;
 	}
 	public void desembarque() {};
-	public void embarqueCartao(String tipoCartao) {}
+	public void embarqueCartao(String tipoCartao) {
+		//registra o embarque de acordo com o tipo de cartao
+	}
 	public void embarque() {};
 	
 }
