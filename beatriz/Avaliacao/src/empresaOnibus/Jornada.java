1ac269bfe2d00510d9a375eb16e2d78fc1c713b5
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-12-04 14:04:13 -0300

    feat:descrição dos métodos

--- beatriz/Avaliacao/src/empresaOnibus/Jornada.java
+++ beatriz/Avaliacao/src/empresaOnibus/Jornada.java
@@ -6,12 +6,20 @@ public class Jornada {
 	private Cobrador cobrador;
 	private int status; //iniciada ou encerrada
 	private Onibus onibus;
+	private String horaInicio;
+	private String horaFinal;
 	//pega a hora que o metodo foi executado
-	public void finalizaJornada() {};
+	public void finalizaJornada() {
+		//finaliza a jornada e coloca a hora Final
+	};
 	
-	//pega a hora que o metodo foi executado
-	public void iniciaJornada(Trajeto trajetoCompleto, Motorista motorista, Cobrador cobrador, Onibus onibus) {}
-	public void iniciaJornada(Trajeto trajetoCompleto, Motorista motorista, Onibus onibus) {}
+	
+	public void iniciaJornada(Trajeto trajetoCompleto, Motorista motorista, Cobrador cobrador, Onibus onibus) {
+		//pega a hora que o metodo foi executado e registra na horaInicial
+	}
+	public void iniciaJornada(Trajeto trajetoCompleto, Motorista motorista, Onibus onibus) {
+		//inicia jornada sem cobrador
+	}
 	
 	
 	
