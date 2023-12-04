fe0b6f717500190e421aade247365924ea1861ad
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-12-04 11:13:09 -0300

    add classe inicial de calculadora P003

---
+++ everaldina/Praticas/src/p003/CalculadoraDivisao.java
@@ -0,0 +1,28 @@
+package p003;
+
+public class CalculadoraDivisao {
+
+    public static void main(String[] args) {
+        
+    }
+
+    // retorna a divisão de dois números
+    public static float dividir(float a, float b) {
+        return a / b;
+    }
+
+    // retorna a soma de dois números
+    public static float somar(float a, float b) {
+        return a + b;
+    }
+
+    // retorna a diferença de dois números
+    public static float subtrair(float a, float b) {
+        return a - b;
+    }
+
+    // retorna o produto de dois números
+    public static float multiplicar(float a, float b) {
+        return a * b;
+    }
+}
